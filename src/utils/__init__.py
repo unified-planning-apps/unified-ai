65ee8fd7101590303df701e64fc57cd72240e726
TOLOJANAHAR <tolojanaharynatachanoeline@gmail.com>
2026-05-13 21:19:37 +0300

    fix(utils):  change constant

---
+++ src/utils/__init__.py
@@ -0,0 +1,22 @@
+"""
+Package utilitaires du projet.
+"""
+
+from src.utils.logger import setup_logging, get_logger
+from src.utils.constants import (
+    UserRole,
+    REGIONS_MADAGASCAR,
+    REGION_NAMES,
+    COULEURS_ALERTE,
+    get_saison_courante,
+)
+
+__all__ = [
+    "setup_logging",
+    "get_logger",
+    "UserRole",
+    "REGIONS_MADAGASCAR",
+    "REGION_NAMES",
+    "COULEURS_ALERTE",
+    "get_saison_courante",
+]
